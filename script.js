6cb6d909451e958c783f75d6287d5a6f157e56d8
Alexis Villarroel <alexralv@gmail.com>
2023-02-26 18:45:34 -0300

    efecto type titulo

---
+++ script.js
@@ -0,0 +1,29 @@
+function initLinkActivo() {
+  const links = document.querySelectorAll('.menu a');
+  if(links.length) {
+    function activarLink(link) {
+      const url = location.href;
+      const href = link.href;
+
+      if(url.includes(href)) {
+        link.classList.add('active')
+      }
+    }
+
+    links.forEach(activarLink)
+  }
+}
+
+initLinkActivo();
+
+
+// typed
+
+var typed = new Typed('.typed', {
+  strings: ['Desarrollo Web^1000 Front-End'],
+  typeSpeed: 100,
+  backSpeed: 50,
+  // loop: true,
+  startDelay: 1000,
+  cursorChar: '_',
+})
